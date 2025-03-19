60c0ef47d8cb09d70b769870eaef8f2cb7bec798
Surya <iamsuryavamsi@gmail.com>
2025-03-19 18:41:59 +0530

    Changes Done

--- src/surya/App.java
+++ src/surya/App.java
@@ -11,21 +11,28 @@ import java.util.TreeSet;
 // Class named with App
 public class App{
 
+    // static final StringBuilder datatype to create new String value
     private static final StringBuilder sb = new StringBuilder();
 
     // Main method and its the starting point of our application
     public static void main(String[] args) {
         
+        // Random array 
         int[] nums = {1, 2, 1, 0, 38, 23, 12};
 
+        // Using try with resources for auto closing if connections are in open state
         try( Scanner scanner = new Scanner(System.in) ){
 
+            // Asking the question to take input from user
             System.out.print("Enter the target to search in our array : ");
 
+            // Taking input from user with Scannner class
             int target = scanner.nextInt();
 
+            // Using the algorithm to search
             int result = binarySearch(nums, target);
 
+            // Printing the correct result with if else statements 
             if ( result != -1 ) {
 
                 if ( sb.toString().equals("LINEAR") ) {
@@ -53,16 +60,20 @@ public class App{
 
         sb.append("LINEAR");
 
+        // Using Set Data Type to avoid duplicates
         Set<Integer> numsSet = new TreeSet<>();
 
+        // Add the nums array to numsSet with enhanced for loop
         for ( int num : nums ){
 
             numsSet.add(num);
 
         }
 
+        // Copying all numsSet to normal List dataype to get index values
         List<Integer> sortedArrayList = new ArrayList<>(numsSet);
 
+        // Now simply giving the index value with traditional for loop with prebuilt method
         return sortedArrayList.indexOf(target);
 
     }
@@ -72,36 +83,46 @@ public class App{
 
         sb.append("BINARY");
 
+        // Using Set Data Type to avoid duplicates
         Set<Integer> numsSet = new TreeSet<>();
 
+        // Add the nums array to numsSet with enhanced for loop
         for ( int num : nums ){
 
             numsSet.add(num);
 
         }
 
+        // Copying all numsSet to normal List dataype to get index values
         List<Integer> sortedArrayList = new ArrayList<>(numsSet);
 
+        // Left value for iteration
         int left = 0;
 
+        // Right value for iteration
         int right = sortedArrayList.size() - 1;
 
+        // While loop to loop without index value
         while( left <= right ){
 
+            // Diving the left and right to get mid value
             int mid = (left + right) / 2;
 
+            // Checking if the mid is equal
             if ( sortedArrayList.get(mid).equals(target) ){
 
                 return mid;
 
             }
 
+            // If the targest is less then mid then we are removing the right part to avoid more steps
             if ( target < sortedArrayList.get(mid) ){
 
                 right = mid - 1;
 
             }
 
+            // If the targest is greater then mid then we are removing the left part to avoid more steps
             if ( target > sortedArrayList.get(mid) ){
 
                 left = mid + 1;
@@ -110,6 +131,7 @@ public class App{
 
         }
 
+        // Returning the false value if no value found 
         return -1;
 
     }
